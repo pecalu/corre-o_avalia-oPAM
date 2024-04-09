70cd4628e8b50ab0b250958b7795a7907e04f6ec
pecalu <124682526+pecalu@users.noreply.github.com>
2024-04-09 12:25:30 -0300

    atividade corrigida

---
+++ MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace avaliacaoPAM
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void rolarDados(object sender, EventArgs e)
+        {
+
+            int selectSides = Convert.ToInt32(SidesPicker.SelectedItem);
+            Random random = new Random();
+            int DrawnNumber = random.Next(1, selectSides+1);
+            ValorSorteado.Text = DrawnNumber.ToString();
+        }
+    }
+
+}
